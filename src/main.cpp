9f1571e7b2a11fb0a3636c3969b7e7dbc4cb1373
Benjamin Man <benjamin.man1001@gmail.com>
2022-08-18 21:27:42 -0700

    add 2022 xdrive code

---
+++ src/main.cpp
@@ -0,0 +1,100 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Ben                                              */
+/*    Created:      Sun Aug 14 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE Controller1FIGURED DEVICES ----
+// Robot Controller1figuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1troller1          Controller1troller                    
+// frontLeft            motor         1               
+// frontRight           motor         2               
+// backLeft             motor         3               
+// backRight            motor         4               
+// ---- END VEXCODE Controller1FIGURED DEVICES ----
+
+#include "vex.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// frontLeft            motor         1               
+// frontRight           motor         5               
+// backLeft             motor         3               
+// backRight            motor         4               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+#include <algorithm>
+#include <cmath>
+using namespace vex;
+
+double normalize(double val){
+  double x = std::pow(val, 2) / 100;
+  if(val < 0){
+    x*=-1;
+  }
+  return x;
+}
+
+int main(void) {
+    while(true) {
+        //Get the raw sums of the X and Y joystick axes
+        double front_left  = (double)(Controller1.Axis1.position(pct) + Controller1.Axis2.position(pct));
+        double back_left   = (double)(Controller1.Axis1.position(pct) - Controller1.Axis2.position(pct));
+        double front_right = (double)(Controller1.Axis1.position(pct) - Controller1.Axis2.position(pct));
+        double back_right  = (double)(Controller1.Axis1.position(pct) + Controller1.Axis2.position(pct));
+        
+        //Find the largest possible sum of X and Y
+        double max_raw_sum = (double)(abs(Controller1.Axis1.position(pct)) + abs(Controller1.Axis2.position(pct)));
+        
+        //Find the largest joystick value
+        double max_XYstick_value = (double)(std::max(abs(Controller1.Axis1.position(pct)),abs(Controller1.Axis2.position(pct))));
+        
+        //The largest sum will be scaled down to the largest joystick value, and the others will be
+        //scaled by the same amount to preserve directionality
+        if (max_raw_sum != 0) {
+            front_left  = front_left / max_raw_sum * max_XYstick_value;
+            back_left   = back_left / max_raw_sum * max_XYstick_value;
+            front_right = front_right / max_raw_sum * max_XYstick_value;
+            back_right  = back_right / max_raw_sum * max_XYstick_value;
+        }
+        
+        //Now to Controller1sider rotation
+        //Naively add the rotational axis
+        front_left  = front_left  + Controller1.Axis3.position(pct);
+        back_left   = back_left   + Controller1.Axis3.position(pct);
+        front_right = front_right - Controller1.Axis3.position(pct);
+        back_right  = back_right  - Controller1.Axis3.position(pct);
+
+        front_left = front_left + Controller1.Axis4.position(pct);
+        back_left = back_left - Controller1.Axis4.position(pct);
+        front_right = front_right + Controller1.Axis4.position(pct);
+        back_right = back_right - Controller1.Axis4.position(pct);
+
+        //What is the largest sum, or is 100 larger?
+        max_raw_sum = std::max(std::abs(front_left),std::max(std::abs(back_left),std::max(std::abs(front_right),std::max(std::abs(back_right),100.0))));
+        
+        //Scale everything down by the factor that makes the largest only 100, if it was over
+        //front right motor is broken
+        double maxpower = 80;
+        front_left  = front_left  / max_raw_sum * maxpower;
+        back_left   = back_left   / max_raw_sum * maxpower;
+        front_right = front_right / max_raw_sum * maxpower;
+        back_right  = back_right  / max_raw_sum * maxpower;
+        
+        front_left = normalize(front_left);
+        back_left = normalize(back_left);
+        front_right = normalize(front_right);
+        back_right = normalize(back_right);
+
+        //Write the manipulated values out to the motors
+        frontLeft.spin(fwd,front_left, velocityUnits::pct);
+        backLeft.spin(fwd,back_left,  velocityUnits::pct);
+        frontRight.spin(fwd,front_right,velocityUnits::pct);
+        backRight.spin(fwd,back_right, velocityUnits::pct);
+    }
+}
