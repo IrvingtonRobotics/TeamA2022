c8d77cb7b56f82a3546db70925f6303626817072
retrostyle012 <69181160+retrostyle012@users.noreply.github.com>
2023-01-10 21:59:29 -0800

    started auton, added helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,308 +6,323 @@
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-
+ 
 // ---- START VEXCODE Controller1FIGURED DEVICES ----
 // Robot Controller1figuration:
 // [Name]               [Type]        [Port(s)]
-// Controller1troller1          Controller1troller                    
-// frontLeft            motor         1               
-// frontRight           motor         2               
-// backLeft             motor         3               
-// backRight            motor         4               
+// Controller1troller1          Controller1troller                   
+// frontLeft            motor         1              
+// frontRight           motor         2              
+// backLeft             motor         3              
+// backRight            motor         4              
 // ---- END VEXCODE Controller1FIGURED DEVICES ----
-
+ 
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// frontLeft            motor         14              
-// frontRight           motor         19              
-// backLeft             motor         20              
-// backRight            motor         17              
-// Intake               motor         16              
-// Flywheel             motor         11              
-// Pusher               motor         18              
-// Flywheel2            motor         2               
+// frontLeft            motor         14             
+// frontRight           motor         19             
+// backLeft             motor         20             
+// backRight            motor         17             
+// Intake               motor         16             
+// Flywheel             motor         4              
+// Pusher               motor         18             
+// Flywheel2            motor         5              
+// Controller1          controller                   
 // ---- END VEXCODE CONFIGURED DEVICES ----
-
+ 
 #include "vex.h"
 #include <algorithm>
 #include <cmath>
 using namespace vex;
-
-double flywheelPow = 100;
+ 
+double flywheelPow = 80;
 // vex::brain::lcd screen;
 std::string forw = "forward";
 std::string back = "backward";
 std::string left = "left";
 std::string rigt = "right";
-
+ 
 double wheelRadius = 3.25 / 2;
 // effective wheel radius since they are all at 45 degree angles
 double effWheelRadius = (wheelRadius / sqrt(2));
 double inchesPerDegree = (2 * M_PI * effWheelRadius) / 360;
-
+ 
 void stop(){
-  frontLeft.stop(); 
-  frontRight.stop();
-  backLeft.stop(); 
-  backRight.stop();
+ frontLeft.stop();
+ frontRight.stop();
+ backLeft.stop();
+ backRight.stop();
 }
-
+ 
 void setPower(double power){
-  frontLeft.setVelocity(power, percent);
-  frontRight.setVelocity(power, percent);
-  backLeft.setVelocity(power, percent);
-  backRight.setVelocity(power, percent);
+ frontLeft.setVelocity(power, percent);
+ frontRight.setVelocity(power, percent);
+ backLeft.setVelocity(power, percent);
+ backRight.setVelocity(power, percent);
 }
-
-void move(std::string dir, double time) {
-  if(dir == "forward"){
-    frontLeft.spin(fwd); 
-    frontRight.spin(reverse);
-    backLeft.spin(fwd); 
-    backRight.spin(reverse);
-  } else if(dir == "right"){ // good
-    frontLeft.spin(fwd); 
-    frontRight.spin(fwd);
-    backLeft.spin(reverse); 
-    backRight.spin(reverse);
-  } else if(dir == "left"){ // good
-    frontLeft.spin(reverse); 
-    frontRight.spin(reverse);
-    backLeft.spin(fwd); 
-    backRight.spin(fwd);
-  } else if(dir == "backward"){
-    frontLeft.spin(reverse); 
-    frontRight.spin(fwd);
-    backLeft.spin(reverse); 
-    backRight.spin(fwd);
-  }
-
-  vex::task::sleep(time);
-  stop();
+ 
+void move(std::string dir, double seconds) {
+ if(dir == "forward"){
+   frontLeft.spin(fwd);
+   frontRight.spin(reverse);
+   backLeft.spin(fwd);
+   backRight.spin(reverse);
+ } else if(dir == "right"){ // good
+   frontLeft.spin(fwd);
+   frontRight.spin(fwd);
+   backLeft.spin(reverse);
+   backRight.spin(reverse);
+ } else if(dir == "left"){ // good
+   frontLeft.spin(reverse);
+   frontRight.spin(reverse);
+   backLeft.spin(fwd);
+   backRight.spin(fwd);
+ } else if(dir == "backward"){
+   frontLeft.spin(reverse);
+   frontRight.spin(fwd);
+   backLeft.spin(reverse);
+   backRight.spin(fwd);
+ }
+ 
+ vex::task::sleep(seconds*1000);
+ stop();
 }
-
+/**
 void move2(std::string dir, double inches) {
-  // frontRight.setReversed(true);
-  // backRight.setReversed(true);
-  double deg = inches / inchesPerDegree;
-  
+ double deg = inches / inchesPerDegree;
   if(dir == "forward"){
-    frontLeft.startRotateFor(deg, degrees);
-    frontRight.startRotateFor(-deg, degrees);
-    backLeft.startRotateFor(deg, degrees);
-    backRight.startRotateFor(-deg, degrees);
-  } else if(dir == "right"){ // good
-    frontLeft.startRotateFor(deg, degrees)
-    frontRight.startRotateFor(deg, degrees)
-    backLeft.startRotateFor(-deg, degrees);
-    backRight.startRotateFor(-deg, degrees);
-  } else if(dir == "left"){ // good
-    frontLeft.startRotateFor(-deg, degrees);
-    frontRight.startRotateFor(-deg, degrees);
-    backLeft.startRotateFor(deg, degrees)
-    backRight.startRotateFor(deg, degrees)
-  } else if(dir == "backward"){
-    frontLeft.startRotateFor(-deg, degrees);
-    frontRight.startRotateFor(deg, degrees)
-    backLeft.startRotateFor(-deg, degrees);
-    backRight.startRotateFor(deg, degrees)
-  }
+   frontLeft.startRotateFor(deg, degrees);
+   frontRight.startRotateFor(-deg, degrees);
+   backLeft.startRotateFor(deg, degrees);
+   backRight.startRotateFor(-deg, degrees);
+ } else if(dir == "right"){ // good
+   frontLeft.startRotateFor(deg, degrees);
+   frontRight.startRotateFor(deg, degrees);
+   backLeft.startRotateFor(-deg, degrees);
+   backRight.startRotateFor(-deg, degrees);
+ } else if(dir == "left"){ // good
+   frontLeft.startRotateFor(-deg, degrees);
+   frontRight.startRotateFor(-deg, degrees);
+   backLeft.startRotateFor(deg, degrees);
+   backRight.startRotateFor(deg, degrees);
+ } else if(dir == "backward"){
+   frontLeft.startRotateFor(-deg, degrees);
+   frontRight.startRotateFor(deg, degrees);
+   backLeft.startRotateFor(-deg, degrees);
+   backRight.startRotateFor(deg, degrees);
+ }
 }
-
+**/
+ 
+ 
 void moveDiag(int dir, double time){
-  /*
-      1     2
-    
-      4     3
-  */
-  switch(dir){
-    case 1:
-      backLeft.spin(fwd);
-      frontRight.spin(reverse);
-      break;
-    case 2:
-      frontLeft.spin(fwd);
-      backRight.spin(reverse);
-      break;
-    case 3:
-      backLeft.spin(reverse);
-      frontRight.spin(fwd);
-      break;
-    case 4:
-      frontLeft.spin(reverse);
-      backRight.spin(fwd);
-      break;
-  }
-
-  vex::task::sleep(time);
-  stop();
-}
-
-void spin(int dir, double time){
-  // time=20 is 90 degrees
-  if(dir == -1){
-    frontLeft.spin(reverse); 
-    frontRight.spin(reverse);
-    backLeft.spin(reverse); 
-    backRight.spin(reverse);
-  }
-  if(dir == 1){
-    frontLeft.spin(fwd); 
-    frontRight.spin(fwd);
-    backLeft.spin(fwd); 
-    backRight.spin(fwd);
-  }
-
-  vex::task::sleep(time);
-  stop();
-}
-
-void flywheelOn(){
-  Flywheel.spin(reverse, flywheelPow, velocityUnits::pct);
-  Flywheel2.spin(reverse, flywheelPow, velocityUnits::pct);
-}
-
-void flywheelOff(){
-  Flywheel.stop();
-  Flywheel2.stop();
-}
-
-void intakeOn(){
-  Intake.spin(fwd, 100, velocityUnits::pct);
-}
-
-void intakeOff(){
-  Intake.stop();
-}
-
-void fireLoad(int times = 1){
-  for(int i=0; i<times; i++){
-    Pusher.spinToPosition(180, degrees);
-    vex::task::sleep(200);
-    Pusher.spinToPosition(0, degrees);
-    vex::task::sleep(500);
-  }
-}
-
-void autonomous(void) {
-  // intakeOn();
-  // move(forw, 2000);
-  // flywheelOn();
-  // vex::task::sleep(2000);
-  // intakeOff();
-  // fireLoad();
-  // vex::task::sleep(1000);
-  // flywheelOff();
-
-  setPower(10);
-  flywheelOn();
-  vex::task::sleep(2000);
-  fireLoad();
-}
-
-void drivercontrol() {
-  // Initialize pusher position
-  Pusher.setPosition(0, degrees);
-  Pusher.setVelocity(100, velocityUnits::pct);
+ /*
+     1     2
   
-  // Driver control loop
-  while(true) {
-    // Get the raw sums of the X and Y joystick axes
-    double front_left  = (double)Controller1.Axis1.position(pct);
-    double back_left   = (double)Controller1.Axis1.position(pct);
-    double front_right = (double)Controller1.Axis1.position(pct);
-    double back_right  = (double)Controller1.Axis1.position(pct);
-    
-    // Find the largest possible sum of X and Y
-    double max_raw_sum = (double)(abs(Controller1.Axis1.position(pct)) + abs(Controller1.Axis2.position(pct)));
-    
-    // Find the largest joystick value
-    double max_XYstick_value = (double)(std::max(abs(Controller1.Axis1.position(pct)),abs(Controller1.Axis2.position(pct))));
-    
-    // The largest sum will be scaled down to the largest joystick value, and the others will be
-    // scaled by the same amount to preserve directionality
-    if (max_raw_sum != 0) {
-      front_left  = front_left / max_raw_sum * max_XYstick_value;
-      back_left   = back_left / max_raw_sum * max_XYstick_value;
-      front_right = front_right / max_raw_sum * max_XYstick_value;
-      back_right  = back_right / max_raw_sum * max_XYstick_value;
-    }
-
-    // Robot controller rotation 
-    // Naively add the rotational axis
-    front_left  += Controller1.Axis3.position(pct);
-    back_left   += Controller1.Axis3.position(pct);
-    front_right -= Controller1.Axis3.position(pct);
-    back_right  -= Controller1.Axis3.position(pct);
-
-    front_left  += Controller1.Axis4.position(pct);
-    back_left   -= Controller1.Axis4.position(pct);
-    front_right += Controller1.Axis4.position(pct);
-    back_right  -= Controller1.Axis4.position(pct);
-
-    // Determine max sum capped 100
-    max_raw_sum = std::max(std::abs(front_left),std::max(std::abs(back_left),std::max(std::abs(front_right),std::max(std::abs(back_right),100.0))));
-    
-    //Scale everything down by the factor that makes the largest only 100, if it was over
-    double maxpower = 80;
-    front_left  = front_left  / max_raw_sum * maxpower;
-    back_left   = back_left   / max_raw_sum * maxpower;
-    front_right = front_right / max_raw_sum * maxpower;
-    back_right  = back_right  / max_raw_sum * maxpower;
-
-    //Write the manipulated values out to the motors
-    frontLeft.spin(fwd,front_left, velocityUnits::pct);
-    backLeft.spin(fwd,back_left,  velocityUnits::pct);
-    frontRight.spin(fwd,front_right,velocityUnits::pct);
-    backRight.spin(fwd,back_right, velocityUnits::pct);
-
-    if(Controller1.ButtonL1.pressing()){
-      Intake.spin(fwd, 100, velocityUnits::pct);
-    } else if(Controller1.ButtonL2.pressing()){
-      Intake.spin(reverse, 100, velocityUnits::pct);
-    } else {
-      Intake.stop();
-    }
-
-
-    if(Controller1.ButtonR1.pressing()){
-      Flywheel.spin(reverse, flywheelPow, velocityUnits::pct);
-      Flywheel2.spin(reverse, flywheelPow, velocityUnits::pct);
-    } else{
-      Flywheel.stop();
-      Flywheel2.stop();
-    }
-
-    if(Controller1.ButtonY.pressing()) {
-      Pusher.spinToPosition(0, degrees);
-    } else {
-      Pusher.spinToPosition(180, degrees);
-    }
-  }
+     4     3
+ */
+ switch(dir){
+   case 1:
+     backLeft.spin(fwd);
+     frontRight.spin(reverse);
+     break;
+   case 2:
+     frontLeft.spin(fwd);
+     backRight.spin(reverse);
+     break;
+   case 3:
+     backLeft.spin(reverse);
+     frontRight.spin(fwd);
+     break;
+   case 4:
+     frontLeft.spin(reverse);
+     backRight.spin(fwd);
+     break;
+ }
+ 
+ vex::task::sleep(time);
+ stop();
 }
-
+ 
+void spin(std::string dir, double time){
+ // time=20 is 90 degrees
+ if(dir == "left"){
+   frontLeft.spin(reverse);
+   frontRight.spin(reverse);
+   backLeft.spin(reverse);
+   backRight.spin(reverse);
+ }
+ if(dir == "right"){
+   frontLeft.spin(fwd);
+   frontRight.spin(fwd);
+   backLeft.spin(fwd);
+   backRight.spin(fwd);
+ }
+ 
+ vex::task::sleep(time);
+ stop();
+}
+ 
+void flywheelOn(){
+ Flywheel.spin(reverse, flywheelPow, velocityUnits::pct);
+ Flywheel2.spin(reverse, flywheelPow, velocityUnits::pct);
+}
+ 
+void flywheelOff(){
+ Flywheel.stop();
+ Flywheel2.stop();
+}
+ 
+void intakeOn(){
+ Intake.spin(fwd, 100, velocityUnits::pct);
+}
+ 
+void intakeOff(){
+ Intake.stop();
+}
+ 
+void shoot(int times = 1){
+ flywheelOn();
+ for(int i=0; i<times; i++){
+   Pusher.spinToPosition(180, degrees);
+   vex::task::sleep(200);
+   Pusher.spinToPosition(0, degrees);
+   vex::task::sleep(500);
+ }
+ flywheelOff();
+}
+ 
+ 
+void sleepSeconds(double seconds){
+ vex::task::sleep(seconds*1000);
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+void autonomous(void) {
+ setPower(100);
+ move("right", 0.1);
+ intakeOn();
+ move("forward", 1);
+ intakeOn();
+ move("left", 0.1);
+ spin("left", 20);
+ move("right", 0.1);
+ sleepSeconds(2);
+ shoot();
+  // move("left/right/forward/backward", 2000);
+ // intakeOn();
+ // intakeOff();
+ // shoot();
+}
+ 
+void drivercontrol() {
+ // Initialize pusher position
+ Pusher.setPosition(0, degrees);
+ Pusher.setVelocity(100, velocityUnits::pct);
+  // Driver control loop
+ while(true) {
+   // Get the raw sums of the X and Y joystick axes
+   double front_left  = (double)Controller1.Axis1.position(pct);
+   double back_left   = (double)Controller1.Axis1.position(pct);
+   double front_right = (double)Controller1.Axis1.position(pct);
+   double back_right  = (double)Controller1.Axis1.position(pct);
+  
+   // Find the largest possible sum of X and Y
+   double max_raw_sum = (double)(abs(Controller1.Axis1.position(pct)) + abs(Controller1.Axis2.position(pct)));
+  
+   // Find the largest joystick value
+   double max_XYstick_value = (double)(std::max(abs(Controller1.Axis1.position(pct)),abs(Controller1.Axis2.position(pct))));
+  
+   // The largest sum will be scaled down to the largest joystick value, and the others will be
+   // scaled by the same amount to preserve directionality
+   if (max_raw_sum != 0) {
+     front_left  = front_left / max_raw_sum * max_XYstick_value;
+     back_left   = back_left / max_raw_sum * max_XYstick_value;
+     front_right = front_right / max_raw_sum * max_XYstick_value;
+     back_right  = back_right / max_raw_sum * max_XYstick_value;
+   }
+ 
+   // Robot controller rotation
+   // Naively add the rotational axis
+   front_left  += Controller1.Axis3.position(pct);
+   back_left   += Controller1.Axis3.position(pct);
+   front_right -= Controller1.Axis3.position(pct);
+   back_right  -= Controller1.Axis3.position(pct);
+ 
+   front_left  += Controller1.Axis4.position(pct);
+   back_left   -= Controller1.Axis4.position(pct);
+   front_right += Controller1.Axis4.position(pct);
+   back_right  -= Controller1.Axis4.position(pct);
+ 
+   // Determine max sum capped 100
+   max_raw_sum = std::max(std::abs(front_left),std::max(std::abs(back_left),std::max(std::abs(front_right),std::max(std::abs(back_right),100.0))));
+  
+   //Scale everything down by the factor that makes the largest only 100, if it was over
+   double maxpower = 80;
+   front_left  = front_left  / max_raw_sum * maxpower;
+   back_left   = back_left   / max_raw_sum * maxpower;
+   front_right = front_right / max_raw_sum * maxpower;
+   back_right  = back_right  / max_raw_sum * maxpower;
+ 
+   //Write the manipulated values out to the motors
+   frontLeft.spin(fwd,front_left, velocityUnits::pct);
+   backLeft.spin(fwd,back_left,  velocityUnits::pct);
+   frontRight.spin(fwd,front_right,velocityUnits::pct);
+   backRight.spin(fwd,back_right, velocityUnits::pct);
+ 
+   if(Controller1.ButtonL1.pressing()){
+     Intake.spin(fwd, 100, velocityUnits::pct);
+   } else if(Controller1.ButtonL2.pressing()){
+     Intake.spin(reverse, 100, velocityUnits::pct);
+   } else {
+     Intake.stop();
+   }
+ 
+ 
+   if(Controller1.ButtonR1.pressing()){
+     Flywheel.spin(fwd, flywheelPow, velocityUnits::pct);
+     Flywheel2.spin(reverse, flywheelPow, velocityUnits::pct);
+   } else{
+     Flywheel.stop();
+     Flywheel2.stop();
+   }
+ 
+   if(Controller1.ButtonY.pressing()) {
+     Pusher.spinToPosition(75, degrees);
+   } else {
+     Pusher.spinToPosition(0, degrees);
+   }
+ }
+}
+ 
 void printFlywheelPow(){
-  Controller1.Screen.clearScreen();
-  Controller1.Screen.setCursor(0, 0);
-  Controller1.Screen.print(flywheelPow);
+ Controller1.Screen.clearScreen();
+ Controller1.Screen.setCursor(0, 0);
+ Controller1.Screen.print(flywheelPow);
 }
 void incrementFlywheelPow(){
-  flywheelPow += 5;
-  printFlywheelPow();
+ flywheelPow += 5;
+ printFlywheelPow();
 }
-
+ 
 void decrementFlywheelPow(){
-  flywheelPow -= 5;
-  printFlywheelPow();
+ flywheelPow -= 5;
+ printFlywheelPow();
 }
-
+ 
 competition Competition;
 int main(void){
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(drivercontrol);
-  Controller1.ButtonUp.pressed(incrementFlywheelPow);
-  Controller1.ButtonDown.pressed(decrementFlywheelPow);
+ Competition.autonomous(autonomous);
+ Competition.drivercontrol(drivercontrol);
+ Controller1.ButtonUp.pressed(incrementFlywheelPow);
+ Controller1.ButtonDown.pressed(decrementFlywheelPow);
 }
